0203b20c63edaadb17ff04b575431fc6951d5c7a
Armend Ceka <armend.ceka@scandiweb.com>
2026-04-21 19:05:42 +0200

    KAN-7 Fixed white homepage screen

--- cabin-frontend/src/layout/MainLayout.jsx
+++ cabin-frontend/src/layout/MainLayout.jsx
@@ -1,10 +1,8 @@
-import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
     <>
-      <Header />
       <main className="mt-20">
         <Outlet />
       </main>
